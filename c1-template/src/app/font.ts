48fe98790a52198bed6c89e6cf2faad384710ddd
your-github-username <your-github-email>
2025-11-15 04:10:32 +0800

    miising

--- c1-template/src/app/font.ts
+++ c1-template/src/app/font.ts
@@ -1,8 +1,6 @@
 import {
   Inter,
   Roboto,
-  Geist,
-  Geist_Mono,
 } from "next/font/google";
 
 export const inter = Inter({
@@ -19,23 +17,7 @@ export const roboto = Roboto({
   variable: "--font-roboto",
 });
 
-export const geist = Geist({
-  subsets: ["latin"],
-  style: ["normal"],
-  display: "swap",
-  variable: "--font-geist",
-});
-
-export const geistMono = Geist_Mono({
-  subsets: ["latin"],
-  style: ["normal"],
-  display: "swap",
-  variable: "--font-geist-mono",
-});
-
 export const fonts = [
   inter,
   roboto,
-  geist,
-  geistMono,
 ];
